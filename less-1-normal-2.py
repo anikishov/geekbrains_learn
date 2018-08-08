b968ce111b124cf0be4c5c145340e70c7160f1e4
anikishov <gryz9ns@gmail.com>
2018-08-08 22:42:50 +0300

    lesson 1 normal  task 2

---
+++ less-1-normal-2.py
@@ -0,0 +1,12 @@
+var1 = int(input("Type here a variable-1:\n"))
+var2 = int(input("Type hare a variable-2:\n"))
+
+var1 += var2
+var2 = var1 - var2
+var1 = var1 - var2 
+
+print("Changed variable-1:\n", var1)
+print("Changed variable-2:\n", var2)
+
+
+
